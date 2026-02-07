ad04c8f6f8cc3ea78a8521e6c0a75b08be42f05c
semi <semiloreoshiyoye@Semilores-MacBook-Pro.local>
2026-02-07 14:28:51 -0500

    log in

--- .next/dev/static/chunks/app_layout_tsx_1cf6b850._.js
+++ .next/dev/static/chunks/app_layout_tsx_1cf6b850._.js
@@ -1,7 +1,9 @@
 (globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/app_globals_71f961d1.css"
+  "static/chunks/node_modules_@clerk_nextjs_dist_esm_app-router_da7351c0._.js",
+  "static/chunks/node_modules_7255179b._.js",
+  "static/chunks/[root-of-the-server]__28bc9c2a._.css"
 ],
     source: "dynamic"
 });
